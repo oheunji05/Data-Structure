f352183fbccb8bf86e425a323827dc5574740ea4
오은지 <81677536+oheunji05@users.noreply.github.com>
2021-09-04 16:13:26 +0900

    [MOVE] repository

---
+++ 파일분할/메인소스.c
@@ -0,0 +1,8 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include "Çì´õ.h"
+//int main() {
+//	extern int a;
+//	abc(5);
+//	printf("extern int a=%d\n", a);
+//	return 0;
+//}
---
+++ 파일분할/함수정의.c
@@ -0,0 +1,7 @@
+//#include "Çì´õ.h"
+//
+//int a = 50;
+//int abc(int n) {
+//	printf("ABC\n");
+//	return 1;
+//}
